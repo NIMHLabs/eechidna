9cc8a9c0e06fe754d4a59ee0f4bcb126b657bf85
Katrina Payne <TheLamia@NIMHLabs.Com>
2014-06-23 23:06:35 -0700

    yeah, lets do this

---
+++ Header/BasicTypes.h
@@ -0,0 +1,47 @@
+/* = eEchidna DOM-Muse Includes/BasicTypes.h
+ * Author: Katrina Payne
+ * Creation Date: 2014 June 23
+ *
+ * This is me working to start on creating a basic implementation of DOM
+ * in the Sybilline Program eEchidna style. This DOM implementation may be
+ * modified to have each part of DOM as a seperate Chromosome.
+ *
+ * == Changelog
+ * * 2014 June 23 -- Created this document
+ *
+ * == TODO
+ * * Make this not completely suck
+ */
+
+
+#ifndef __eEchidna_DOM_Muse_Basic_Type_H__
+#define __eEchidna_DOM_Muse_Basic_Type_H__
+/* Begin header file */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* Blot out C++ */
+
+/* Just flat out ripping off the standard documents at this point */
+typedef short* edna_DOMString;
+typedef unsigned long long edna_DOMTimeStamp
+typedef void* edna_DOMUserData;
+typedef void* edna_DOMObject;
+/* Most of this will be re written to not be filled with suck, and still
+ * work with what W3C has written up... or at least not be too evil*/
+
+/* Wait... is this the file?
+ * Oh dear sweet merciless Eris... 
+ * This is soooo gonna be written later on when I don't 
+ * suck at coding as much
+ * (Watch as this is used in coding studies in 2152CE as a cryptic message
+ * that current computer users don't understand as this code is still in use
+ * at that time) */
+
+
+#ifdef __cplusplus
+}
+#endif /* Code C in the shade*/
+
+#endif /* The file */
--- README.md
+++ README.md
@@ -3,4 +3,25 @@ eechidna
 
 The Mother of All Coding Project Monsters
 
-Going to begin tossing in crappy parts of this. It will eventually improve as things go along.
+Going to begin tossing in crappy parts of this. It will eventually improve as 
+things go along.
+
+-=-
+
+Here is the DOM-Muse branch. Keeping with the Greek Diamones/Monster theme of 
+naming, I will be having various prerecongised data holding systems that are
+common enough to require some kind of specialised handling to be listed as
+Muses within this system.
+
+There will likely be a PDF-Muse, a Parrot-Muse, a JVM-Muse, a CSM-Muse, 
+a Adobe-Flash-Muse (if possible), ObjC-Muse in amongst anything else that has 
+me trying to make use of somebody else's standard/system for keeping stuff 
+straight all up in this biotch.
+
+Any virtual machine, documentation handling format, etc that will be based 
+upon my own design (taking in information from the others) will likely be
+call a Sibylline-Machine. Technically, eEchidna in and of itself will a form
+of Sibylline Machine in its grand function... as a lot of the code is designed
+to handle situations that do not exist yet, and to be patched based upon 
+new knowledge of those situations that have not existed yet being experienced
+to help those parts work better.
